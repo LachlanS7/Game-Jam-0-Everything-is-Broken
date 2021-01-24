8767a34716a2c70ebe9eb71b53ed156084d4bb21
LachlanS7 <lwschill73@gmail.com>
2021-01-25 00:10:39 +1030

    Item

---
+++ Assets/Scripts/Item.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var sprite = $Sprite
+
+var item_id : int
+
+func set_item(id: int):
+	pass
+	#item_id = id
+	
+	#sprite.texture = owner.get_node("Player")
+	#get_node("Sprite")
